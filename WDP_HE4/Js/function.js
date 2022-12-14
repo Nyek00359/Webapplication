b311960134d8565ede763bd65bfff422b5703a24
Nyek00359 <118493585+Nyek00359@users.noreply.github.com>
2022-12-14 14:18:31 +0800

    Add files via upload

---
+++ WDP_HE4/Js/function.js
@@ -0,0 +1,69 @@
+var count = 4;
+function validate(){
+        var un = document.myform.username.value;
+        var pw = document.myform.password.value;
+        var valid = false;
+        var login = false;
+        var unArray = ['glenn','claire'];  // as many as you like - no comma after final entry
+        var pwArray = ['terrible','salazar'];  // the corresponding passwords;
+
+        for (var i=0; i <unArray.length; i++) {
+                if ((un == unArray[i]) && (pw == pwArray[i])) {
+                        valid = true;
+                        login = true;
+                        break;
+                }
+        }
+        if (valid) {
+                alert ("Login was sucessfully processed.  You will be redirected to the members page now.");
+                        window.sessionStorage.setItem('user',un);
+                        window.sessionStorage.setItem('login',login);
+                        window.location= "Profile/profile.html";
+                        document.myform.username.value = "";
+                        document.myform.pword.value = "";
+                        setTimeout("document.myform.username.focus()", 25);
+                        setTimeout("document.myform.username.select()", 25);
+                        count --;
+                        return false;
+                }
+        else{
+                login = false;
+        }
+        var t = " tries";
+        if (count == 1) {t = " try"}
+
+        if (count >= 1) {
+                alert ("Invalid username and/or password.  You have " + count + t + " left.");
+                document.myform.username.value = "";
+                document.myform.pword.value = "";
+                setTimeout("document.myform.username.focus()", 25);
+                setTimeout("document.myform.username.select()", 25);
+                count --;
+        }
+
+        else {
+                alert ("Still incorrect! You have no more tries left!");
+                document.myform.username.value = "No more tries allowed!";
+                document.myform.pword.value = "";
+                document.myform.username.disabled = true;
+                document.myform.pword.disabled = true;
+                return false;
+}
+
+}
+
+function User(){
+
+        var user = sessionStorage.getItem('user');
+        var login = sessionStorage.getItem('login');
+if(login){
+  window.location= user+".html";
+}
+else{
+        window.location="../register.html";
+}
+};
+
+function Logon(){
+        
+}
